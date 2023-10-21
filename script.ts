7ef3baa2fb62aa66b8f88c2509ff5dbddbf2d5f3
José Luiz Júnior <30448740+Joseluizdantasjr@users.noreply.github.com>
2023-10-21 18:52:35 -0300

    Update script.ts

--- script.ts
+++ script.ts
@@ -19,29 +19,30 @@ const prisma = new PrismaClient()
 async function main() {
   // ... you will write your Prisma Client queries here
 
-  // roda os alunos
+  // Criar os alunos
   const primeiro_aluno = new Aluno("Guilherme", 1909, "guilherme@powerrangers.com", "Ciencia da Computacao");
   const segundo_aluno = new Aluno("Bernardo Abutre", 23, "kkkkeusoualto@chama.br", "Gastronomia");
   const terceiro_aluno = new Aluno("Christopher", 44, "sumiu@cadeatete.com", "O que der na telha");
 
 
-  // Rodando o professor
+  // Criar o professor
   const primeiro_professor = new Professor("Bianca", "peteleu@peteleco.pt", 1011);
 
-  // Rodando o estande
+  // Criar o estande
   const primeiro_estande = new Estande(1)
 
-  // Rodando o grupo
+  // Criar o grupo
   const primeiro_grupo = new Grupo("Os Farofeiros", primeiro_aluno.retornarMatricula(), 1, primeiro_estande.retornarNumero(), new Date());
 
 
-  // Avaliações 
+  // Criar as avaliações 
   const primeira_avaliacao_aluno = new AvaliacaoAluno(1, terceiro_aluno.retornarMatricula(), primeiro_grupo.retornarNumero(), primeiro_aluno.fazerAvaliacao(1, 7));
 
   const primeira_avaliacao_professor = new AvaliacaoProfessor(1, primeiro_professor.retornarMatricula(), primeiro_grupo.retornarNumero(), primeiro_professor.fazerAvaliacao(1,8));
 
   const primeira_avaliacao = new Avaliacao(1, primeira_avaliacao_aluno.retornarNumero(), primeira_avaliacao_professor.retornarNumero(), primeiro_grupo.retornarNumero());
-  
+
+  //Para rodar os códigos de preencher o prisma com esses dados, copiar e rodar aqui cada um dos códigos de população especificados no readme
 }
 
 
